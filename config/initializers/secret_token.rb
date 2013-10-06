1f9d428a50280960bc466a4009225df8d0d4b429
Anisul <anisul.huq90@gmail.com>
2013-10-06 01:07:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chonkey::Application.config.secret_key_base = '8fc3e0db6b05f2678096f14990fffaf84ed660a0143bef824ea4ca6ca2042a943541d092e5bed66bb232e6094e5897455134800583de6488d9bf6d8037ceb09a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chonkey::Application.config.session_store :cookie_store, key: '_chonkey_session'
